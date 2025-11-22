7155d22eb57dc32c13f11561724d2abcbf02f74d
trungbui12 <yuuichirou67@gmail.com>
2025-11-22 09:02:20 +0700

    BEupdate

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,2 +1,52 @@
 import User from "../models/User.js";
 import { Webhook } from "svix";
+
+const clerkWebhooks = async (req, res)=>{
+    try {
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const hearders = {
+            "svix-id": req.hearders["svix-id"],
+            "svix-timestamp": req.hearders["svix-timestamp"],
+            "svix-signature": req.hearders["svix-signature"],
+        };
+        // verifying headers
+        await whook.verify(JSON.stringify(req.body), headers)
+
+        // getting data from request body
+        const {data, type} = req.body
+
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + " " + data.last_name,
+            image: data.image_url,
+        }
+
+        // switch cases for different events
+        switch (type){
+            case "user.created": {
+                await User.create(userData);
+                break;
+            }
+
+            case "user.updated":{
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+            }
+
+            case "user.deleted":{
+                await User.findByIdAndDelete(data.id);
+                break;
+            }
+
+            default:
+                break;
+        }
+        res.json({success: true, message: "Webhook Recieved"})
+    } catch (error){
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export default clerkWebhooks;
--- server/server.js
+++ server/server.js
@@ -3,14 +3,19 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB()
 
 const app = express()
 app.use(cors()) // Bật chia sẻ tài nguyên giữa các nguồn khác nhau
 
+// middleware
 app.use(express.json())
 app.use(clerkMiddleware())
+
+// api to listen to clerk webhooks
+app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res)=> res.send("API is working fine "))
 
